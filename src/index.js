ec7c68fd07064fffae59a5e7571d0c435b609487
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-08-01 01:35:38 +0200

    feat: add soltion to the index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
+nmodule.exports = function reverse (n) {
+  let rev = ''; 
+  let def = String(n)   
+  if (n > 0) {
+  for (let i = 0; i < def.length; i++) {
+    rev = def[i] + rev;
+  }} else {
+    for (let i = 1; i < def.length; i++) {
+        rev = def[i] + rev;
+      }}
   
+  return Number(rev);
 }
